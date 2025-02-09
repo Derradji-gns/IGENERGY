8f916b8e48b63cdb6f1f85e76b5bffe37b238438
٠Derradji Amine abdelbasset <derradjiamineabdelbasset@gmail.com>
2025-02-09 22:54:43 +0100

    Update header.jsx

--- my-club/src/header.jsx
+++ my-club/src/header.jsx
@@ -2,13 +2,14 @@ import './header.css';
 import Aos from 'aos';
 import "aos/dist/aos.css"
 import { useEffect } from 'react';
+import model1 from './graphics/model1.jpg';
 function Header( ) {
 useEffect(()=>{
     Aos.init({
     })
 })
     return(
-        <div className="Hparent" style={{backgroundImage: `url(/graphics/model1.jpg)`}}>
+        <div className="Hparent" style={{backgroundImage: `url(${model1})`}}>
                         <div className="son">
                 <h1>IGENERGY Student's CLUB </h1>
                 <p>Igenergy Students' Club at ENSTTIC is a student organization focused on technology, innovation, and skill development. It offers activities like coding workshops, chess competitions, and tech events to enhance members' technical and teamwork abilities, creating a dynamic community.</p>
