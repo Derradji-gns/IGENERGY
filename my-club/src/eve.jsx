70ccaf9d28c2aa76cb2edeee899907e3f05a8c1d
Derradji-gns <derradjiamineabdelbasset@gmail.com>
2025-02-10 20:57:04 +0100

    modifying

--- my-club/src/eve.jsx
+++ my-club/src/eve.jsx
@@ -7,10 +7,17 @@ function OurEvents() {
         <div className="Fchap">
                       <div className="title"><h2>OUR EVENTS</h2></div>
             <div className="cont">
+
                 <Card value="Tech hub :" desc="Great competition for programmers and IT students" img="./graphics/techhub.jpg" />
                 <Card value="IT Day :" desc="Event organized by the us featuring tech talks, workshops, and innovative projects." img="./graphics/it.jpg"/>
                 <Card value="Energy Space :" desc="A space for communication and sharing electronic projects between members" img="./graphics/space.jpg"/>
                 <Card value="Speak Up :" desc="Great day to meet new members and help them get to know the school and the club." img="./graphics/tech-logo.jpg"/>
+
+                <Card value="Tech hub :" desc="Great competition for programmers and IT students" img="./graphics/techhub.jpg"/>
+                <Card value="IT Day :" desc="Event organized by us featuring tech talks, workshops, and innovative projects." img="./graphics/it.jpg"/>
+                <Card value="Energy Space :" desc="A space for communication and sharing electronic projects between members" img="./graphics/space.jpg"/>
+                <Card value="Speak Up :" desc="An event to boosts public speaking skills through talks and interactive discussions" img="./graphics/speak-logo.png"/>
+ 7046b4d (save modifing project)
                 <Card value="Teach Me How ?" desc="Workshops and an opportunity to develop club members and acquire new skills." img="./graphics/lteach.jpg"/>
                 <Card value="Hour Of Code :" desc="Teaching members programming languages and problem solving" img="./graphics/tech-logo.jpg"/>
             </div>
