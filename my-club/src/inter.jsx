4de469e0c8e7aa2893f1876c33e27d05dbc67236
٠Derradji Amine abdelbasset <derradjiamineabdelbasset@gmail.com>
2025-02-10 18:11:04 +0100

    Update inter.jsx

--- my-club/src/inter.jsx
+++ my-club/src/inter.jsx
@@ -9,14 +9,14 @@ function OurInterests(){
                 <p>We can proudly say that what distinguishes us from other scientific clubs is our interests:</p>
             </div>
             <div className="conten">
-                <Papier title="Programming" img="src/graphics/svg1.svg"/>
-                <Papier title="Design" img="src/graphics/ldesign.svg"/>
-                <Papier title="Social Media" img="src/graphics/socia.svg"/>
-                <Papier title="Podcasting" img="src/graphics/podcasting.svg"/>
-                <Papier title="Public Speaking" img="src/graphics/speak.svg"/>
-                <Papier title="Coding" img="src/graphics/lcode.svg"/>
-                <Papier title="Gamming" img="src/graphics/play.svg"/>
-                <Papier title="Hiking" img="src/graphics/hik.svg"/>
+                <Papier title="Programming" img="./graphics/svg1.svg"/>
+                <Papier title="Design" img="./graphics/ldesign.svg"/>
+                <Papier title="Social Media" img="./graphics/socia.svg"/>
+                <Papier title="Podcasting" img="./graphics/podcasting.svg"/>
+                <Papier title="Public Speaking" img="./graphics/speak.svg"/>
+                <Papier title="Coding" img="./graphics/lcode.svg"/>
+                <Papier title="Gamming" img="./graphics/play.svg"/>
+                <Papier title="Hiking" img="./graphics/hik.svg"/>
 
             </div>
             
